4dadb491d6628faa16951658c070cc350fe3a7ac
Shaheen Siddiqui <shanvisiddya@gmail.com>
2024-01-04 09:20:14 +0530

    feat: made the field required

--- models/product/validetor.js
+++ models/product/validetor.js
@@ -4,12 +4,13 @@ const productSchema = Joi.object({
   title: Joi.string().required(),
   price: Joi.number().required(),
   rating: Joi.number().required(),
-  category: Joi.string(),
-  imageUrl: Joi.string(),
+  category: Joi.string().required(),
+  imageUrl: Joi.string().required(),
   
-  smallSize: Joi.array().items(Joi.number()), // Array of numbers
-  availableColor: Joi.array().items(Joi.string()), // Array of strings
-  crouselImage: Joi.array().items(Joi.string()), // Array of strings
+  smallSize: Joi.array().items(Joi.number()).min(1).required(), // Array of numbers, at least one item required
+  availableColor: Joi.array().items(Joi.string()).min(1).required(), // Array of strings, at least one item required
+  crouselImage: Joi.array().items(Joi.string()).min(1).required(), // Array of strings, at least one item required
+
 });
 
 module.exports = productSchema;
